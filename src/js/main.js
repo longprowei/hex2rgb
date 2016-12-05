6d083809c1527c985431fef95444ad34ae1c0506
Chenglong Wei <longprowei@gmail.com>
2016-12-05 22:57:24 +1100

    add js file

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $(".hex-color").keypress(function(e) {
+    });
+
+    $('.rgb-color').keypress(function(e) {
+    });
+
+    function matchHex(str) {
+    }
+
+    function matchRgb(str) {
+    }
+});
