41321771ef0886880ce73f41151b2e3416ad48f5
Chenglong Wei <longprowei@gmail.com>
2016-12-08 23:24:43 +1100

    add empty functions

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,40 @@
-$(document).ready(function() {
-    $(".hex-color").change(function() {
-        matchHex(this.value);
+(function(){
+    $(document).ready(function() {
+        $(".hex-color").keypress(function() {
+            if (matchHex($(this).val())) {
+                var rgbStr = hexToRgb(processHex(this.value));
+                this.val(rgbStr);
+            }
+        });
+
+        $('.rgb-color').keypress(function(e) {
+            if (matchRgb($(this).val())) {
+                var hexStr = rgbToHex(processRgb(this.value));
+                this.val(hexStr);
+            }
+        });
+
+        function matchHex(str) {
+            console.log(str);
+            str = str.trim();
+        }
+
+        function matchRgb(str) {
+            console.log(str);
+            str = str.trim();
+        }
+
+        function processHex(str) {
+        }
+
+        function processRgb(str) {
+        }
+
+        function hexToRgb(str) {
+        }
+
+        function rgbToHex(str) {
+        }
+        
     });
-
-    $('.rgb-color').keypress(function(e) {
-        matchRgb(this.value);
-    });
-
-    function matchHex(str) {
-        str = trim(str);
-    }
-
-    function matchRgb(str) {
-    }
-});
+})();
